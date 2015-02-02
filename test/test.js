d19937e9652206c849a32d38019a57033c7e078e
Mikola Lysenko <mikolalysenko@gmail.com>
2015-02-02 16:52:13 -0600

    adding test cases

---
+++ test/test.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var tape = require('tape')
+var createTurntable = require('../turntable')
+
+tape('turntable-controller', function(t) {
+
+  var camera = createTurntable()
+
+
+
+  t.end()
+})
